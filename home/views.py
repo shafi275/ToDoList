04fba61c2460df55b90d35980f8f2bb7ea159344
shafi <shariyarshafi27@gmail.com>
2025-04-19 00:58:58 +0600

    saving info in db

--- home/views.py
+++ home/views.py
@@ -1,9 +1,19 @@
+
 from django.shortcuts import render
 from django.conf import settings
-
-
+from home.models import Task  # assuming you’ll be saving tasks later
 
 def home(request):
+    if request.method == "POST":
+        taskName = request.POST.get('taskName')
+        taskDesc = request.POST.get('taskDesc')
+        print(taskDesc, taskName)
+        ins=Task(tasktitle=taskName ,taskdesc= taskDesc )
+        ins.save()
+        
+        # You can also save the task if needed:
+        # Task.objects.create(name=taskName, description=taskDesc)
     return render(request, 'index.html')
+
 def tasks(request):
     return render(request, 'tasks.html')
