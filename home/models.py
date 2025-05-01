37352ed9e2baf0e0ba8229ec5d473cf4d712887f
shafi <shariyarshafi27@gmail.com>
2025-05-01 13:15:04 +0600

    trying to implment register and login part

--- home/models.py
+++ home/models.py
@@ -1,6 +1,7 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 class Task(models.Model):
+    user=models.ForeignKey(User,on_delete=models.SET_NULL,null=True,blank=True)
     tasktitle = models.CharField(max_length=30)
     taskdesc = models.TextField()
     time = models.DateTimeField(auto_now_add=True)
